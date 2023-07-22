b307f8e1940783634c7faf933c01d4c3cbeedeaa
Parwat Kunwar <parwatkunwar08@gmail.com>
2023-07-22 18:38:23 -0400

    take full height for list

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,13 +2,23 @@ import './sidebar.css';
 import Menu from './menu';
 import ProjectMenu from './ProjectMenu';
 import Widget from './Widget';
+import LeftArrow from '../images/icons/left-arrow.svg'
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className='logo-container'>
-        <img id='logo' src='https://raw.githubusercontent.com/parwatcodes/TaskMan/main/public/images/logo.png' alt='logo' />
+      <div style={{
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+      <img id='logo' src='https://raw.githubusercontent.com/parwatcodes/TaskMan/main/public/images/logo.png' alt='logo' />
         <h3>Task Man</h3>
+      </div>
+
+        <div>
+          <img src={LeftArrow} alt='hide-sidebar' />
+        </div>
       </div>
       <Menu />
       <ProjectMenu />
